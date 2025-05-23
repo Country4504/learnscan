ba2f2e2f6482a9cbff9d4c4ccb1f4d47f40bc3fb
gwl <jkuufo@gmail.com>
2025-05-23 21:12:57 +0800

    Admin Page

--- docker/mongo-init.js
+++ docker/mongo-init.js
@@ -5,12 +5,16 @@ db = db.getSiblingDB('learning_scan');
 db.createCollection('users');
 db.createCollection('surveys');  // 改为与 mongoose 模型匹配的名称
 db.createCollection('reports');  // 添加报告集合
+db.createCollection('admins');
 
 // 创建索引
 db.users.createIndex({ "username": 1 }, { unique: true });
 db.surveys.createIndex({ "user": 1 });
 db.reports.createIndex({ "user": 1 });
 db.reports.createIndex({ "survey": 1 });
+db.admins.createIndex({ "username": 1 }, { unique: true });
 
 // 打印确认信息
-print('MongoDB 初始化完成'); 
+console.log('数据库初始化完成');
+console.log('创建了以下集合: users, surveys, reports, admins');
+console.log('创建了相应的索引'); 
